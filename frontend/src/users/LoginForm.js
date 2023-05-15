0b3fce5b7df64ced565607b2271f80113d0a7633
Zane Matero <materozane@gmail.com>
2023-05-15 11:51:24 -0700

    practice with sessions, commited with error

--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -20,6 +20,7 @@ function LoginForm() {
     async function handleSubmit(e) {
         const response = await fetch(`http://localhost:5000/authentication/`, {
             method: 'POST',
+            credentials: 'include',
             headers: {
                 'Content-Type': 'application/json'
             },
